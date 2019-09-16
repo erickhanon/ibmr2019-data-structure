6344a7127d96786e796b351f6ab12cf77644e116
erkanon <50847394+erkanon@users.noreply.github.com>
2019-09-16 18:48:13 -0300

    Update ex_05.java

--- src/controller/ex_05.java
+++ src/controller/ex_05.java
@@ -6,7 +6,7 @@ public class ex_05 {
 
     public static double getValidGrade() {
         double grade = Teclado.lerDouble("");
-        if (grade > 0 && grade < 10) return grade;
+        if (grade >= 0 && grade <= 10) return grade;
         System.out.print("Nota inválida! Tente novamente: ");
 
         return getValidGrade();
